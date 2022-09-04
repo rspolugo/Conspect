8e6404aa4fed52868a0806b4e005e8bcd13493c6
evgeniichernigovskii <evgeniic@seriously.com>
2022-09-04 19:40:45 +0300

    added requirements.txt

--- chapter_5_functions/functions.py
+++ chapter_5_functions/functions.py
@@ -1,13 +1,14 @@
 import random
-MIN = 1
-MAX = 6
-def main():
-    again = 'да'
-    while again == 'да':
-        print('бросем кубики')
-        print('значение граней')
-        print(random.randint(MIN, MAX))
-        print(random.randint(MIN, MAX))
-        again = input('бросить еще раз?: да/нет ')
 
-main()
+
+def main(MIN: int, MAX: int):
+    again = "да"
+
+    while again == "да":
+        print("бросем кубики\nзначение граней")
+        print(random.randint(MIN, MAX))
+        print(random.randint(MIN, MAX))
+        again = input("бросить еще раз?: да/нет ")
+
+
+main(1, 6)
